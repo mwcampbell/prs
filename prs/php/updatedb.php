8a14683d60ea8711dbd274a2ef737faa12367817
Matt Campbell <mattcampbell@pobox.com>
2002-12-20 18:14:15 +0000

    Added line breaks in the form.
    git-svn-id: svn://svn.code.sf.net/p/prsradio/code/trunk@125 410355f2-c4ea-45de-a5b5-301721f902b9

--- prs/php/updatedb.php
+++ prs/php/updatedb.php
@@ -7,8 +7,9 @@ html_start ("Update Database");
 if (!$audio_root) {
 ?>
 <form name = "update_db" action="updatedb.php" method="post">
+<div>
 <label for="audio_root">Enter the root directory containing the audio files you wish to add/update:</label>
-<input type = "text" name="audio_root" id="audio_root">
+<input type = "text" name="audio_root" id="audio_root"><br>
 <label for="existing_category">Select a category in which to place all files under this directory:</label>
 <select name = "existing_category" id="existing_category">
 <?
@@ -20,10 +21,11 @@ if (!$audio_root) {
 	}
         mysql_free_result ($res);
 ?>
-</select>
+</select><br>
 <label for="new_category">Or enter a new category in which to place the files:</label>
-<input type = "text" name="new_category" id="new_category">
+<input type = "text" name="new_category" id="new_category"><br>
 <input type = "submit" value="Start Update">
+</div>
 </form>
 <?
 }
@@ -34,12 +36,11 @@ else {
 		$commandline .= "-c \"".$existing_category."\" ";
 	else
 		$commandline .= "-c \"".$new_category."\" ";;
-	$commandline .= " \"".$audio_root."\" >& /dev/null &";
+	$commandline .= " \"".$audio_root."\" >& /dev/null";
 	exec ($commandline);
-echo "Returned $return\n";
 ?>
-Database update in progress...
-<a href = "main.php">Back to main Administration page</a>
+<p>Database update in progress...</p>
+<div><a href = "main.php">Back to main menu</a></div>
 <?
 }
 html_end ();
