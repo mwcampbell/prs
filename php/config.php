9d4657152a61b518916005d639e1ec4e41a2bf8b
Geoff Shang <geoff@QuiteLikely.com>
2007-09-28 15:23:08 +0000

    php/config.php: Reformat config.php to make it easier to read and understand.  Also see next commit.
    git-svn-id: svn://svn.code.sf.net/p/prsradio/code/trunk@341 410355f2-c4ea-45de-a5b5-301721f902b9

--- php/config.php
+++ php/config.php
@@ -1,3 +1,9 @@
 <?
-$STATIONS = array ("test" => "/home/marc/prs.conf");
+// Define your PRS stations and save as config.php.
+// You need a comma after each entry except the last one.
+
+$STATIONS = array (
+	"test" => "/home/marc/prs.conf",
+	"test 2" => "/home/geoff/prs.conf"
+);
 ?>
