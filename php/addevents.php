72f897a24461030795ce4b685175eedb27a9bf97
Geoff Shang <geoff@QuiteLikely.com>
2007-09-29 19:04:19 +0000

    php/addevents.php: More register_globals work and readability fixes.
    git-svn-id: svn://svn.code.sf.net/p/prsradio/code/trunk@344 410355f2-c4ea-45de-a5b5-301721f902b9

--- php/addevents.php
+++ php/addevents.php
@@ -5,36 +5,47 @@ db_connect ();
 
 /* Ensure that $template_events_repeat contains something */
 
-if (!$template_events_repeat)
+if (!$_POST["template_events_repeat"])
 	$template_events_repeat = "0";
+else $template_events_repeat = $_POST["template_events_repeat"];
 
 /* Connect to the prs database */
 
-if ($new_template)
+if ($_POST["new_template"])
 {
 
 	list ($artist_exclude_hours, $artist_exclude_minutes) =
-		explode (":", $template_artist_exclude, 2);
+		explode (":", $_POST["template_artist_exclude"], 2);
 	$artist_exclude = $artist_exclude_hours*3600+$artist_exclude_minutes*60;
 	list ($recording_exclude_hours, $recording_exclude_minutes) =
-		explode (":", $template_recording_exclude, 2);
+		explode (":", $_POST["template_recording_exclude"], 2);
 	$recording_exclude = $recording_exclude_hours*3600+$recording_exclude_minutes*60;
 
-	$query = "insert into playlist_template (template_name, repeat_events, handle_overlap, artist_exclude, recording_exclude) values ('" . addslashes ($template_name) . "', $template_events_repeat, $template_handle_overlap, $artist_exclude, $recording_exclude)";
+	$query = "insert into playlist_template (template_name, repeat_events, handle_overlap, artist_exclude, recording_exclude) values ('" . addslashes ($_POST["template_name"]) . "', $template_events_repeat, $_POST["template_handle_overlap"], $artist_exclude, $recording_exclude)";
 	db_query ($query);
 	$template_id = mysql_insert_id ();
-}
+} else if ($_SERVER["REQUEST_METHOD"] == "GET")
+	$template_id = $_GET["template_id"];
+else $template_id = $_POST["template_id"];
+
+// At this point, $template_id has the ID of the template, either from MySQL,
+// from a form, or from createvent.php via a redirect (which uses "get").
 
 html_start ("Add Playlist Event");
-?>
-<ul>
-<li><a href="randomevent.php?template_id=<? echo $template_id ?>&amp;event_number=<? echo $event_number ?>&amp;insert_event=<? echo $insert_event ?>&amp;event_type=random">Add Random Event</a></li>
-<li><a href="randomevent.php?template_id=<? echo $template_id ?>&amp;event_number=<? echo $event_number ?>&amp;insert_event=<? echo $insert_event ?>&amp;event_type=simple_random">Add Simple Random Event</a></li>
-<li><a href="urlevent.php?template_id=<? echo $template_id ?>&amp;event_number=<? echo $event_number ?>&amp;insert_event=<? echo $insert_event ?>">Add URL Event</a></li>
-<li><a href="pathevent.php?template_id=<? echo $template_id ?>&amp;event_number=<? echo $event_number ?>&amp;insert_event=<? echo $insert_event ?>">Add Path Event</a></li>
-<li><a href = "edittemplate.php?template_id=<? echo $template_id ?>">Edit this template</a></li>
-<li><a href = "template.php">Back to Playlist Template Administration</a></li>
-</ul>
-<?
+
+// Do this to avoid multiple long lines.
+$url_query_base = 'template_id=' . $template_id .
+	'&amp;event_number=' . $_POST["event_number"] .
+	'&amp;insert_event=' . $_POST["insert_event"];
+
+echo "<ul>\n";
+echo '<li><a href="randomevent.php?' . $url_query_base . '&amp;event_type=random">Add Random Event</a></li>' . "\n";
+echo '<li><a href="randomevent.php?' . $url_query_base . '&amp;event_type=simple_random">Add Simple Random Event</a></li>' . "\n";
+echo '<li><a href="urlevent.php?' . $url_query_base . '">Add URL Event</a></li>' . "\n";
+echo '<li><a href="pathevent.php?' . $url_query_base . '">Add Path Event</a></li>' . "\n";
+echo '<li><a href = "edittemplate.php?template_id=' . $template_id . '">Edit this template</a></li>' . "\n";
+echo '<li><a href = "template.php">Back to Playlist Template Administration</a></li>' . "\n";
+echo "</ul>\n";
+
 html_end ();
 ?>
