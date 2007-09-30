2ad1084eea104ba386e55ab58b48daa262b69d68
Geoff Shang <geoff@QuiteLikely.com>
2007-09-30 20:14:33 +0000

    php/createevent.php: Update to work with register_globals=off.  All template manipulation scripts should now work without register_globals. Scheduling scripts still don't work yet.
    git-svn-id: svn://svn.code.sf.net/p/prsradio/code/trunk@351 410355f2-c4ea-45de-a5b5-301721f902b9

--- php/createevent.php
+++ php/createevent.php
@@ -5,35 +5,47 @@ db_connect ();
 
 /* Ensure that event_anchor_position has something in it */
 
-if (!$event_anchor_position)
+if ($_POST["event_anchor_position"])
+	$event_anchor_position = $_POST["event_anchor_position"]
+else
 	$event_anchor_position = "0";
 
-if (!$event_channel_name)
-	$event_channel_name = $event_name;
+if ($_POST["event_channel_name"])
+	$event_channel_name = $_POST["event_channel_name"]
+else
+	$event_channel_name = $_POST["event_name"];
 
-if ($insert_event) {
+if ($_POST["insert_event"]) {
 	/* Move existing events to make room */
 
-	$query = "update playlist_event set event_number = event_number+1 where event_number >= $event_number and template_id = $template_id";
+	$query = "update playlist_event set event_number = event_number+1 where " .
+	"event_number >= " . $_POST["event_number"] . " and template_id = " . $_POST["template_id"];
 	db_query ($query);
 }
 
 /* See if event exists */
 
-$query = "select * from playlist_event where event_number = $event_number and template_id = $template_id";
+$query = "select * from playlist_event where event_number = " . $_POST["event_number"] .
+	" and template_id = " . $_POST["template_id"];
 $res = db_query ($query);
 if (mysql_numrows ($res) > 0) {
-	$query = "update playlist_event
-        set template_id = $template_id, event_number = $event_number,
-        event_name = \"$event_name\", event_type = \"$event_type\",
-        event_channel_name = \"$event_channel_name\", event_level = \"$event_level\",
-        event_anchor_event_number = $event_anchor_event_number,
-        event_anchor_position = $event_anchor_position,
-        event_offset = $event_offset,
-        detail1 = \"$detail1\", detail2 = \"$detail2\",
-        detail3 = \"$detail3\", detail4 = \"$detail4\",
-        detail5 = \"$detail5\"
-        where template_id = $template_id and event_number = $event_number";
+	$query = 'update playlist_event ' .
+        'set template_id = ' . $_POST["template_id"] . ', ' .
+	'event_number = ' . $_POST["event_number"] . ', ' .
+        'event_name = "' . $_POST["event_name"] . '", ' .
+	'event_type = "' . $_POST["event_type"] . '", ' .
+        'event_channel_name = "' . $event_channel_name . '", ' .
+	'event_level = "' . $_POST["event_level"] . '", ' .
+        'event_anchor_event_number = ' . $_POST["event_anchor_event_number"] . ', ' .
+        'event_anchor_position = ' . $event_anchor_position . ', ' .
+        'event_offset = ' . $_POST["event_offset"] . ', ' .
+        'detail1 = "' . $_POST["detail1"] . '", ' .
+	'detail2 = "' . $_POST["detail2"] . '", ' .
+        'detail3 = "' . $_POST["detail3"] . '", ' .
+	'detail4 = "' . $_POST["detail4"] . '", ' .
+        'detail5 = "' . $_POST["detail5"] . '" ' .
+        'where template_id = ' . $_POST["template_id"] . ' ' .
+	'and event_number = ' . $_POST["event_number"];
 }
 else {
 	$query = "insert into playlist_event
@@ -45,22 +57,21 @@ else {
 	event_offset,
 	detail1, detail2,
 	detail3, detail4,
-	detail5) values
-	($template_id, $event_number,
-	'$event_name', '$event_type',
-	'$event_channel_name', $event_level,
-	$event_anchor_event_number,
-	$event_anchor_position,
-	$event_offset,
-	'$detail1', '$detail2',
-	'$detail3', '$detail4',
-	'$detail5')";
+	detail5) values (" . 
+	$_POST["template_id"] . ', ' . $_POST["event_number"] . ', ' .
+	'"' . $_POST["event_name"] . '", "' . $_POST["event_type"] . '", ' .
+	'"' . $event_channel_name . '", ' . $_POST["event_level"] . ', ' .
+	$_POST["event_anchor_event_number"] . ', ' .
+	$event_anchor_position . ', ' . $_POST["event_offset"] . ', ' .
+	'"' . $_POST["detail1"] . '", "' . $_POST["detail2"] . '", ' .
+	'"' . $_POST["detail3"] . '", "' . $_POST["detail4"] . '", ' .
+	'"' . $_POST["detail5"] . ')';
 }
 db_query ($query);
-if ($update_event || $insert_event) {
-	redirect ("edittemplate.php?template_id=$template_id");
+if ($_POST["update_event"] || $_POST["insert_event"]) {
+	redirect ("edittemplate.php?template_id=" . $_POST["template_id"]);
 }
 else {
-	redirect ("addevents.php?template_id=$template_id");
+	redirect ("addevents.php?template_id=" . $_POST["template_id"]);
 }
 ?>
